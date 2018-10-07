c207beb02d0f587001e6c1e62a83475c6f545f6f
Janic Duplessis <janicduplessis@gmail.com>
2018-10-07 14:45:43 -0400

    Remove regenerator runtime

--- packages/react-native/Libraries/Core/InitializeCore.js
+++ packages/react-native/Libraries/Core/InitializeCore.js
@@ -31,7 +31,6 @@ require('../../src/private/core/setUpDOM');
 require('./setUpPerformance');
 require('./setUpErrorHandling');
 require('./polyfillPromise');
-require('./setUpRegeneratorRuntime');
 require('./setUpTimers');
 require('./setUpXHR');
 require('./setUpAlert');
