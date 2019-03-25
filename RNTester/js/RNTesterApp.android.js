709d4e3556e0c9ac1bc10aedb8c3d852aba45f99
Janic Duplessis <janicduplessis@gmail.com>
2019-03-25 00:12:00 -0400

    New LayoutContext API to access the frame and safe area of the root view

--- RNTester/js/RNTesterApp.android.js
+++ RNTester/js/RNTesterApp.android.js
@@ -123,8 +123,8 @@ class RNTesterApp extends React.Component<Props, RNTesterNavigationState> {
            * found when making Flow check .android.js files. */
           this.drawer = drawer;
         }}
-        renderNavigationView={this._renderDrawerContent}
-        statusBarBackgroundColor="#589c90">
+        renderNavigationView={this._renderDrawerContent}>
+        <StatusBar backgroundColor="#589c90" />
         {this._renderApp()}
       </DrawerLayoutAndroid>
     );
@@ -266,7 +266,6 @@ const styles = StyleSheet.create({
   },
   drawerContentWrapper: {
     flex: 1,
-    paddingTop: StatusBar.currentHeight,
     backgroundColor: 'white',
   },
 });
